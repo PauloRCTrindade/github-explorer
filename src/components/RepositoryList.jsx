0c7eebb0fb6708e70e63cd7c23d4ec846a585b79
Paulo R C Trindade <Paulinhotrindade121212@gmail.com>
2021-11-03 17:31:34 -0300

    Create RepositoryList.jsx

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,42 @@
+export function RepositoryList(){
+	return (
+		<section className="repositoryList">
+			<h1>Lista de Repositórios</h1>
+
+			<ul>
+				<li>
+					<strong>unform</strong>
+					<p>Forms in React</p>
+
+					<a href="">
+						Aceessar repositórios
+					</a>
+				</li>
+			</ul>
+
+			<ul>
+				<li>
+					<strong>unform</strong>
+					<p>Forms in React</p>
+
+					<a href="">
+						Aceessar repositórios
+					</a>
+				</li>
+			</ul>
+
+			<ul>
+				<li>
+					<strong>unform</strong>
+					<p>Forms in React</p>
+
+					<a href="">
+						Aceessar repositórios
+					</a>
+				</li>
+			</ul>
+
+
+		</section>
+	);
+}
